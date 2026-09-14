006dac52b1a504da07e0b5aee35558e1e45acbd5
Mag Core <achillejcristof@gmail.com>
2026-09-14 12:16:35 +0200

    Update robots rules and sitemap URL

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: '*', allow: '/', disallow: ['/private/','/admin/'] },
-    sitemap: 'https://mag-core-v07.vercel.app/sitemap.xml',
+    rules: { userAgent: '*', allow: '/' },
+    sitemap: 'https://mag-core-v08.vercel.app/sitemap.xml',
   }
 }
