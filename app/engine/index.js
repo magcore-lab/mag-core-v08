68793fab4a2816bfbfdfd2fa9df73222a94293d1
Mag Core <achillejcristof@gmail.com>
2026-09-19 18:43:41 +0200

    Initialize channels in DmxEngine constructor

--- app/engine/index.js
+++ app/engine/index.js
@@ -1,6 +1,5 @@
-
 // @ts-nocheck
 export class QuantumCore{constructor(){this.device=null}async init(){return null}tick(){return null}destroy(){}}
-export class DmxEngine{constructor(){}async init(){return null}tick(){return null}destroy(){}}
+export class DmxEngine{constructor(){this.channels=new Array(512).fill(0)}async init(){return null}tick(){return null}destroy(){}}
 export class UnifiedControlBus{constructor(){this.engines=[]}async init(){return null}register(e){}tick(){}destroy(){}}
 export default {}
