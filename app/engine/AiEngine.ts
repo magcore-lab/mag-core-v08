20323e764b38123c5445bc0bd2adf9b72da66d70
Mag Core <achillejcristof@gmail.com>
2026-09-19 15:50:56 +0200

    Refactor AiEngine texture initialization and add tick method

--- app/engine/AiEngine.ts
+++ app/engine/AiEngine.ts
@@ -1,10 +1,12 @@
+
 export class AiEngine{
-  private t:GPUTexture|null=null
+  t:GPUTexture|null=null
   async init(d:GPUDevice){
-    this.t=d.createTexture({size:[256,256,1],format:'rgba8unorm',usage:GPUTextureUsage.TEXTURE_BINDING|GPUTextureUsage.COPY_DST|GPUTextureUsage.RENDER_ATTACHMENT})
-    d.queue.writeTexture({texture:this.t},new Uint8Array(256*256*4),{bytesPerRow:1024},[256,256,1])
+    const s:GPUExtent3D=[256,256,1]
+    this.t=d.createTexture({size:s,format:"rgba8unorm",usage:GPUTextureUsage.TEXTURE_BINDING|GPUTextureUsage.COPY_DST|GPUTextureUsage.RENDER_ATTACHMENT})
+    d.queue.writeTexture({texture:this.t},new Uint8Array(256*256*4),{bytesPerRow:1024},s)
     return this.t
   }
-  compute(){}
+  tick(){}
   destroy(){try{this.t?.destroy()}catch{}}
 }
