5f65a35ec65bfd8eba216af7a7d890c2496fa197
Mag Core <achillejcristof@gmail.com>
2026-09-23 11:19:44 +0200

    Integrate CoreVR component into the page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-
 'use client'
+import dynamic from 'next/dynamic'
+
+const CoreVR = dynamic(() => import('./CoreVR'), { ssr: false })
+
 export default function Page(){
-  return <div style={{background:'#000',width:'100vw',height:'100vh',margin:0}} />
+  return (
+    <div style={{background:'#000',width:'100vw',height:'100vh',margin:0,overflow:'hidden'}}>
+      <CoreVR />
+    </div>
+  )
 }
